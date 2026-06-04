7097609d9f28aba79b8c03662ffae74ffd444ab9
zonghhh <192726425+zonghhh@users.noreply.github.com>
2026-06-04 19:23:27 +0800

    fix: greeter initialization cmd

--- modules/services/greeter/greeter.nix
+++ modules/services/greeter/greeter.nix
@@ -8,12 +8,7 @@
     services.greetd = {
       enable = true;
       settings.default_session = {
-        command = ''
-          ${pkgs.tuigreet}/bin/tuigreet \
-            --time \
-            --remember \
-            --cmd "uwsm start hyprland-uwsm.desktop"
-        '';
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
         user = "greeter";
       };
     };
