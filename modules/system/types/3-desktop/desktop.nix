640250c4f0e22546b42a6bbc42354441c5722628
zonghhh <192726425+zonghhh@users.noreply.github.com>
2026-06-04 18:50:41 +0800

    fix: add substitute for hyprland binaries

--- modules/system/types/3-desktop/desktop.nix
+++ modules/system/types/3-desktop/desktop.nix
@@ -4,6 +4,11 @@
       system-default
     ];
 
+    nix.settings = {
+      substituters        = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+
     programs.hyprland = {
       enable        = true;
       withUWSM      = true;
