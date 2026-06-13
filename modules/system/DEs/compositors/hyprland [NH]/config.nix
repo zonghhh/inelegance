d398182bee4899c1675cb71d19c6cb78cf448ef4
zonghhh <192726425+zonghhh@users.noreply.github.com>
2026-06-13 19:34:10 +0800

    feat: hyprland binds

--- modules/system/DEs/compositors/hyprland [NH]/config.nix
+++ modules/system/DEs/compositors/hyprland [NH]/config.nix
@@ -1,10 +1,48 @@
 { ... }: {
-  # Hyprland Lua is still recent, we do it like this and wait for better alternative.
+  # TODO: Hyprland Lua is still recent, we do it like this and wait for better alternative.
   flake.modules.homeManager.hyprland.wayland.windowManager.hyprland.extraConfig = /* lua */ ''
     local mod = "SUPER"
+    local shiftMod = mod .. " + SHIFT"
 
     -- BINDS --
+
+    -- Quick-Launch --
     hl.bind(mod .. " + Return", hl.dsp.exec_cmd("ghostty"))
+    hl.bind(mod .. " + SPACE", hl.dsp.global("caelestia:launcher")) -- Launcher
+    hl.bind(mod .. " + X", hl.dsp.global("caelestia:session")) -- Powermenu
+    hl.bind(mod .. " + N", hl.dsp.exec_cmd("caelestia shell drawers toggle sidebar")) -- Sidebar (notifications, quick actions)
+    hl.bind(mod .. " + D", hl.dsp.exec_cmd("caelestia shell drawers toggle dashboard")) -- Dashboard
+
+    -- Windows --
+    hl.bind(mod .. " + Q", hl.dsp.window.close())
+    hl.bind(mod .. " + F", hl.dsp.window.fullscreen({ action = "toggle" }))
+    hl.bind(mod .. " + V", hl.dsp.window.float({ action = "toggle" })) -- Toggle floating
+
+    -- mouse
+    hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true }) -- Move window
+    hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true }) -- Resize window
+
+    -- Focus Windows --
+    hl.bind(mod .. " + Left", hl.dsp.focus({ direction = "left" }))
+    hl.bind(mod .. " + Down", hl.dsp.focus({ direction = "down" }))
+    hl.bind(mod .. " + Up", hl.dsp.focus({ direction = "up" }))
+    hl.bind(mod .. " + Right", hl.dsp.focus({ direction = "right" }))
+    hl.bind(shiftMod .. " + J", hl.dsp.layout("removemaster")) -- Remove from master
+    hl.bind(shiftMod .. " + K", hl.dsp.layout("addmaster")) -- Add to master
+
+    -- Utilities --
+    hl.bind(shiftMod .. " + S", hl.dsp.global("caelestia:screenshotFreeze")) -- Capture region (freeze)
+
+    -- Locked Binds --
+    -- brightness
+    hl.bind("XF86MonBrightnessUp", hl.dsp.global("caelestia:brightnessUp"), { locked = true })
+    hl.bind("XF86MonBrightnessDown", hl.dsp.global("caelestia:brightnessDown"), { locked = true })
+
+    -- sound
+    hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true })
+    hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true })
+    hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { locked = true })
+
 
     -- MONITORS --
     hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
