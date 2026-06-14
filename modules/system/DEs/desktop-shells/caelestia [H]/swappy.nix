1c682d3ccac820f4b978f20112bc3d88f09a4132
zonghhh <192726425+zonghhh@users.noreply.github.com>
2026-06-14 20:54:38 +0800

    feat: swappy config

---
+++ modules/system/DEs/desktop-shells/caelestia [H]/swappy.nix
@@ -0,0 +1,11 @@
+{ ... }: {
+  # swappy is the annotation editor caelestia's screenshot flow opens.
+  flake.modules.homeManager.caelestia.xdg.configFile."swappy/config".text = ''
+    [Default]
+    save_dir=~/Pictures/Screenshots
+    save_filename_format=screenshot-%Y%m%d-%H%M%S.png
+    show_panel=false
+    line_size=5
+    text_size=20
+  '';
+}
